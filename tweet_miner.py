009910a5fb80b51f294c8c7bf6c7491afd161a64
rjadrich <rjadrich@gmail.com>
2016-07-10 11:04:09 -0500

    added tweet extraction

--- tweet_miner.py
+++ tweet_miner.py
@@ -2,39 +2,44 @@ import time
 import pandas as pd
 import tweepy
 import boto3
+import os
 
 def fetch_tweets():
     #key information to access the twitter api
-    consumer_key = "IUZ7bZtjQmhtbh36FY4RtIqY4"
-    consumer_secret = "fEYROmCU5WTInSQOYoqLLo2x5CiagQnMNu2oLEPVoUraIfh4Cq"
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    api = tweepy.API(auth)
+    #consumer_key = "IUZ7bZtjQmhtbh36FY4RtIqY4"
+    #consumer_secret = "fEYROmCU5WTInSQOYoqLLo2x5CiagQnMNu2oLEPVoUraIfh4Cq"
+    #consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
+    #consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+    #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    #api = tweepy.API(auth)
 
     #keywords to search for
     keywords = ['ablaze', 'accident', 'aftershock', 'airplane accident', 'ambulance', 'annihilated', 'annihilation', 'apocalypse', 'armageddon', 'army', 'arson', 'arsonist', 'attack', 'attacked', 'avalanche', 'battle', 'bioterror', 'bioterrorism', 'blaze', 'blazing', 'bleeding', 'blew up', 'blight', 'blizzard', 'blood', 'bloody', 'blown up', 'body bag', 'body bagging', 'body bags', 'bomb', 'bombed', 'bombing', 'bridge collapse', 'buildings burning', 'buildings on fire', 'burned', 'burning', 'burning buildings', 'bush fires', 'casualties', 'casualty', 'catastrophe', 'catastrophic', 'chemical emergency', 'cliff fall', 'collapse', 'collapsed', 'collide', 'collided', 'collision', 'crash', 'crashed', 'crush', 'crushed', 'curfew', 'cyclone', 'damage', 'danger', 'dead', 'death', 'deaths', 'debris', 'deluge', 'deluged', 'demolish', 'demolished', 'demolition', 'derail', 'derailed', 'derailment', 'desolate', 'desolation', 'destroy', 'destroyed', 'destruction', 'detonate', 'detonation', 'devastated', 'devastation', 'disaster', 'displaced', 'drought', 'drown', 'drowned', 'drowning', 'dust storm', 'earthquake', 'electrocute', 'electrocuted', 'emergency', 'emergency plan', 'emergency services', 'engulfed', 'epicentre', 'evacuate', 'evacuated', 'evacuation', 'explode', 'exploded', 'explosion', 'eyewitness', 'famine', 'fatal', 'fatalities', 'fatality', 'fear', 'fire', 'fire truck', 'first responders', 'flames', 'flattened', 'flood', 'flooding', 'floods', 'forest fire', 'forest fires', 'hail', 'hailstorm', 'harm', 'hazard', 'hazardous', 'heat wave', 'hellfire', 'hijack', 'hijacker', 'hijacking', 'hostage', 'hostages', 'hurricane', 'injured', 'injuries', 'injury', 'inundated', 'inundation', 'landslide', 'lava', 'lightning', 'loud bang', 'mass murder', 'mass murderer', 'massacre', 'mayhem', 'meltdown', 'military', 'mudslide', 'natural disaster', 'nuclear disaster', 'nuclear reactor', 'obliterate', 'obliterated', 'obliteration', 'oil spill', 'outbreak', 'pandemonium', 'panic', 'panicking', 'police', 'quarantine', 'quarantined', 'radiation emergency', 'rainstorm', 'razed', 'refugees', 'rescue', 'rescued', 'rescuers', 'riot', 'rioting', 'rubble', 'ruin', 'sandstorm', 'screamed', 'screaming', 'screams', 'seismic', 'sinkhole', 'sinking', 'siren', 'sirens', 'smoke', 'snowstorm', 'storm', 'stretcher', 'structural failure', 'suicide bomb', 'suicide bomber', 'suicide bombing', 'sunk', 'survive', 'survived', 'survivors', 'terrorism', 'terrorist', 'threat', 'thunder', 'thunderstorm', 'tornado', 'tragedy', 'trapped', 'trauma', 'traumatised', 'trouble', 'tsunami', 'twister', 'typhoon', 'upheaval', 'violent storm', 'volcano', 'war zone', 'weapon', 'weapons', 'whirlwind', 'wild fires', 'wildfire', 'windstorm', 'wounded', 'wounds', 'wreck', 'wreckage', 'wrecked']
+    
+    #key information to access the twitter api
+    consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
+    consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    api = tweepy.API(auth)
 
-    #get the tweets and place in a dataframe
+    #get the tweets and store in a list
     new_tweets = []
     for keyword in ["accident"]: #cleaned_keywords:
         results = api.search(q = keyword)
         for result in results:
             new_tweets.append(result.text.encode('ascii', errors ="ignore"))
-         
-    new_tweets_df = pd.DataFrame(data = new_tweets, columns = ["text"])  
-        
-    #create a time stamp to aid in identifying the most recent dataset of the two stored 
-    ticks = time.time()  
     
-    #place into a dataframe and write a csv file with utf8 encoded tweets 
+    #create a pandas dataframe of the new tweets
+    new_tweets_df = pd.DataFrame(data = new_tweets, columns = ["text"])  
+    
+    #write a csv file with utf8 encoded tweets 
     new_tweets_df = pd.DataFrame(data = new_tweets, columns = ["text"])
     new_tweets_df.to_csv(path_or_buf = 'data/tweets.csv')
     
-    #testing aws
+    #the heroku filesystem is ephermeral so this file must be moved to amazon web services s3 hosting
     s3client = boto3.client('s3')
-    object_key = 'python_sample_key_2222.txt'
-    bucket_name = 'disasters-on-twitter'
-    s3client.put_object(Bucket=bucket_name, Key=object_key, Body=b'Hello World!')
+    object_key = '%i.csv' % int(time.time())
+    bucket_name = os.environ["S3_BUCKET_NAME"]
     s3client.upload_file('data/tweets.csv', Bucket=bucket_name, Key=object_key)
-
         
     return new_tweets
